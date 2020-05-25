810f224e3694242fa3e43871023f694778a43f00
lone-DO <lotussythe@gmail.com>
2020-05-24 22:25:01 -0400

    Renaming App

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace ReactViewer
+namespace RedditViewer
 {
     public class Program
     {
