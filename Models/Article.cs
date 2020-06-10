a928e4a5b49bf697e34d93180973be0762aa7ee0
lone-DO <lotussythe@gmail.com>
2020-06-10 18:54:23 -0400

    Better JSX styled body

--- Models/Article.cs
+++ Models/Article.cs
@@ -8,6 +8,7 @@ namespace RedditViewer
     {
         public static string Article(JToken data)
         {
+
             string card = "";
             var Title = data["data"]["title"];
             var Author = data["data"]["author"];
@@ -18,50 +19,27 @@ namespace RedditViewer
             var Thumbnail = data["data"]["title"];
             var Score = data["data"]["score"];
             var numComments = data["data"]["numComments"];
-
-            card += $"<article id=\"{ID}card\">";
-            card += $"<div id=\"Article-sidebar\" >";
-            card += $"<button class=\"votebutton\" id=\"upvote\">UP</button>";
-            card += $"<span id=\"Article.Score\">{Score}</span>";
-            card += $"<button class=\"votebutton\" id=\"downvote\">DOWN</button>";
-            card += $"</div>";
-            card += $"<div id=\"Article-content\">";
-            card += $"<div id=\"Article.information\">";
-            card += $"<i id=\"Article.Author\">Posted by {Author}</i>";
-            card += $"<i id=\"Article.Date\">{Utc} days ago</i>";
-            card += $"</div>";
-            card += $"<p id=\"Article.Title\">{Title}</p>";
-            card += $"<img src={Url}>";
-            card += $"<div id=\"Article-footer\">";
-            card += $"<span id=\"Article.numComments\">{numComments} Comments</span>";
-            card += $"<span>Share</span>";
-            card += $"</div>";
-            card += $"</div>";
-            card += $"</article>";
-
-            // RENDER JSON INTO HTML SNIPPETS, 
-
-            /*RETURN HTML
-            <article class="Article.ID card">
-                <div id="Article-sidebar">
-                    <button class="votebutton" id="upvote">UP</button>
-                    <span id="Article.Score"></span>
-                    <button class="votebutton" id="downvote">DOWN</button>
+            card += $@"
+            <article id='{ID} card'>
+                <div id='Article-sidebar'>
+                    <button class='votebutton' id='upvote'>UP</button>
+                    <span id='Article.Score'>{Score}</span>
+                    <button class='votebutton' id='downvote'>DOWN</button>
                 </div>
-                <div id="Article-content">
-                    <div id="Article.information">
-                        <i id="Article.Author">Posted by @*Article.Author*@</i>
-                        <i id="Article.Date">@*Article.Date*@ 2 days ago</i>
+                <div id='Article-content'>
+                    <div id='Article.information'>
+                        <i id='Article.Author'>Posted by {Author}</i>
+                        <i id='Article.Date'>{Utc} days ago</i>
                     </div>
-                    <p id="Article.Title">@*Article.Title*@</p>
-                    <img src="Article.Url">
-                    <div id="Article-footer">
-                        <span id="Article.numComments">@*Article.numComments*@ Comments</span>
+                    <p id='Article.Title'>{Title}</p>
+                    <img src={Url}>
+                    <div id='Article-footer'>
+                        <span id='Article.numComments'>{numComments} Comments</span>
                         <span>Share</span>
                     </div>
                 </div>
             </article>
-            */
+            ";
             return card;
         }
     }
