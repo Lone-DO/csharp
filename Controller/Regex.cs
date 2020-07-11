71be52bd9e5ebe033ecc0c4acbf141a4864c632f
lone-DO <lotussythe@gmail.com>
2020-07-11 05:16:26 -0400

    updated Regex urls, removed uneeded file

--- Controller/Regex.cs
+++ Controller/Regex.cs
@@ -7,8 +7,8 @@ namespace RedditClientViewer.Data
             "New line [https://imgur.com/kKtnL12](https://imgur.com/kKtnL12)"
             Title = [https://imgur.com/kKtnL12]
             Src = https://imgur.com/kKtnL12
-            URL_REGEX Test: https://regex101.com/r/MiGGnF/3
-            TITLE_REGEX Test: https://regex101.com/r/MiGGnF/4
+            URL_REGEX Test: https://regex101.com/r/MiGGnF/7
+            TITLE_REGEX Test: https://regex101.com/r/MiGGnF/6
         **/
         public static RegexOptions OPTIONS = RegexOptions.Multiline | RegexOptions.IgnoreCase;
         public static string URL_REGEX = @"(ht)(.)+?:(\/){2}?(\w)+.(\w)+(\/)?(\w)+.(\w)+(\/?)(\w?)+(\.?)(\w?)+";
